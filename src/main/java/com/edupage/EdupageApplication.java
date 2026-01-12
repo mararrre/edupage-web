a0fc11d8d6a69d98aaaa84959de12e29d156376e
AlexeyMiagkikh <alexmyagkikh0@gmail.com>
2026-01-12 10:44:02 +0600

    feat: setup project structure and User entity

---
+++ src/main/java/com/edupage/EdupageApplication.java
@@ -0,0 +1,13 @@
+package com.edupage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdupageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdupageApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/edupage/model/Role.java
@@ -0,0 +1,7 @@
+package com.edupage.model;
+
+public enum Role {
+    ADMIN,
+    TEACHER,
+    STUDENT
+}
